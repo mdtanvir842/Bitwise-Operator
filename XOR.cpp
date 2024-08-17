2f0a2a73ec15320719cb4a7a3525899f83e6fdb2
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-08-18 02:37:19 +0600

    Create XOR.cpp

---
+++ XOR.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+    int a,b,c;
+    //cin>>a>>b;
+    cin>>a>>b>>c;
+    //cout<<(a^b)<<endl;
+    cout<<(a^b^c)<<endl;
+    //different then one.same then zero.
+
+}
